46396a03437d9df47dd9a8ce19ba278e3b81f5af
Dare Ololade <oladare186@gmail.com>
2026-01-04 16:02:06 +0100

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import noteRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
@@ -11,15 +12,26 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5001
 
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin: "http://localhost:5173"
 }))
+}
+
 app.use(express.json())
 app.use(rateLimiter) 
+const __dirname = path.resolve()
 
 
 app.use("/api/notes", noteRoutes)
 
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
+}
 connectDB().then(() => {
     app.listen(5001, () => {
         console.log(`Server Started on port: ${PORT}`)
